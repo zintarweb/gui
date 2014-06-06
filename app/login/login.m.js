7cae67554a8bad0108c392203fea2d3e4175c3d7
John Lee <zintarweb@gmail.com>
2014-06-06 16:26:07 -0700

    recommit

---
+++ app/login/login.m.js
@@ -0,0 +1,22 @@
+thisApp.factory('loginFactory', function ($http, Session) {
+  return {
+    login: function (credentials) {
+      return $http
+        .post(config.wsUrl + '/login', credentials)
+        .then(function (res) {
+          Session.create(res.id, res.userid, res.role);
+        });
+    },
+    isAuthenticated: function () {
+      return !!Session.userId;
+    },
+    isAuthorized: function (authorizedRoles) {
+      if (!angular.isArray(authorizedRoles)) {
+        authorizedRoles = [authorizedRoles];
+      }
+      return (this.isAuthenticated() &&
+        authorizedRoles.indexOf(Session.userRole) !== -1);
+    }
+  };
+});
+
